dcaf5a71536ddf09c90adbab4062e19a05751db9
Aman Baloch <aman.baloch@amido.com>
2018-08-07 11:46:53 +0100

    add typescript client

---
+++ SignalRChat/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, "wwwroot/spa"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/spa"
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(["wwwroot/spa/*"]),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[chunkhash].css"
+        })
+    ]
+};
